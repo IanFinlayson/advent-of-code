852feb0291e19491fb1adc192489e092fcd193fe
Ian Finlayson <ifinlay@umw.edu>
2024-12-12 13:04:05 -0500

    Commiting this attempt at part 2...but it's not the way...

--- 12/soln.py
+++ 12/soln.py
@@ -11,7 +11,6 @@ def getInput():
             grid.append(row[:-1])
     return grid
 
-
 def floodID(crop, grid, regions, row, col, id):
     # if out of bounds
     if row < 0 or row >= len(grid) or col < 0 or col >= len(grid[0]):
@@ -32,11 +31,10 @@ def floodID(crop, grid, regions, row, col, id):
     floodID(crop, grid, regions, row, col - 1, id)
     floodID(crop, grid, regions, row, col + 1, id)
 
-
 # replaces letters with region ids, 0+
 def findRegions(grid):
     regions = [[-1 for i in range(len(grid[0]))] for j in range(len(grid))]
-
+    symbols = []
     nextrow, nextcol = 0, 0
     nextid = 0
     done = False
@@ -44,6 +42,7 @@ def findRegions(grid):
     while not done:
         # flood fill nextrow, nextcol
         floodID(grid[nextrow][nextcol], grid, regions, nextrow, nextcol, nextid)
+        symbols.append(grid[nextrow][nextcol])
 
         # get next stuff
         isNext = False
@@ -63,7 +62,7 @@ def findRegions(grid):
             done = True
         else:
             nextid += 1
-    return regions, nextid + 1
+    return regions, nextid + 1, symbols
 
 # returns -1 for out-of-bounds to simplify checks
 def r(regions, row, col):
@@ -97,6 +96,7 @@ def part1(regions, num):
         cost += area[i] * perim[i]
     return cost
 
+# get top cell of a region by id
 def getTopCell(regions, id):
     for row in range(len(regions)):
         for col in range(len(regions[0])):
@@ -116,8 +116,8 @@ def dest(regions, id, row, col, dir):
             return row - 1, col
 
 # for part 2, we need the number of sides
-# to do this, we find the top/left most cell and walk the perimiter until we get back
-def numSides(regions, id):
+# this finds the number of _exterior_ sides by using a left-hand-path walk of the wall
+def exteriorSides(regions, id):
     sides = 0
     startrow, startcol = getTopCell(regions, id)
     dir = "E"
@@ -125,7 +125,6 @@ def numSides(regions, id):
     row, col = startrow, startcol
 
     while (row != startrow or col != startcol) or sides < 4:
-        #print("(", row, ", ", col, ") ", dir, sep="")
         # take one step, we always want to turn left to hug the perimiter
         # we figure out our preferred directions based on that
         match dir:
@@ -141,7 +140,6 @@ def numSides(regions, id):
         # try to turn left
         leftrow, leftcol = dest(regions, id, row, col, prefs[0])
         if r(regions, leftrow, leftcol) == id:
-            #print("Left turn")
             row, col = leftrow, leftcol
             dir = prefs[0]
             sides += 1
@@ -150,12 +148,10 @@ def numSides(regions, id):
         # try to go straight
         straightrow, straigthcol = dest(regions, id, row, col, prefs[1])
         if r(regions, straightrow, straigthcol) == id:
-            #print("Straight")
             row, col = straightrow, straigthcol
             continue
 
         # turn right in place
-        #print("Right turn")
         dir = prefs[2]
         sides += 1
 
@@ -171,13 +167,51 @@ def numSides(regions, id):
             sides += 2
     return sides
 
-def part2(regions, num):
+
+def lockedIn(regions, id):
+    row, col = getTopCell(regions, id)
+    #TODO
+    return True
+
+
+# we now need to find the overall number of sides to a region
+# based on exterior AND interior ones
+def numSides(regions, id, symbol):
+    # step one, find the exterior sides using above function
+    exteriors = exteriorSides(regions, id)
+
+    # now we make a new grid with this one having its symbol
+    # and all others being a different symbol *
+    # that way we treat them all as equal and look for interior regisons
+    newgrid = [["*" for i in range(len(regions[0]))] for j in range(len(regions))]
+    for row in range(len(regions)):
+        for col in range(len(regions[0])):
+            if regions[row][col] == id:
+                newgrid[row][col] = symbol[id]
+            print(newgrid[row][col], end="")
+        print()
+
+    newRegions, newNum, newSymbols = findRegions(newgrid)
+    interiorSides = 0
+    
+    # for each of these new regions, see if we can reach the border of the map or not
+    # if we can't it's exteriorSides must be added to the OVERALL ones sides
+    for region in range(newNum):
+        # if it's the same thingy, then no
+        if newSymbols[region] == symbol:
+            continue
+
+        if lockedIn(newRegions, region):
+            print("Adding in", exteriorSides(newRegions, region))
+            interiorSides += exteriorSides(newRegions, region)
+    
+    # now we know total number of sides
+    return exteriors + interiorSides
+
+def part2(regions, num, symbols):
     # area is same, but now we get sides instead of perim
     area = [0 for i in range(num)]
-    sides = [numSides(regions, i) for i in range(num)]
-
-    # TODO we need to adjust this for regions entirely inside of others
-    # those interior ones need their side counts added to the enclosing one ...
+    sides = [numSides(regions, i, symbols[i]) for i in range(num)]
 
     for row in range(len(regions)):
         for col in range(len(regions[0])):
@@ -193,10 +227,11 @@ def part2(regions, num):
 
 
 grid = getInput()
-regions, num = findRegions(grid)
+regions, num, symbols = findRegions(grid)
+print(symbols)
 print("There are", num, "regions")
-cost = part2(regions, num)
-print(cost)
+#cost = part2(regions, num, symbols)
+#print(cost)
 
-
+print(numSides(regions, 0, symbols[0]))
 
