a6c1f4093b2db1ec082b1a5b192a5ba913dae835
Ian Finlayson <ifinlay@umw.edu>
2025-12-11 11:45:31 -0500

    Got part 1 done.  Part 2 way too slow, maybe infinite??

---
+++ 2025/day11/part1.py
@@ -0,0 +1,56 @@
+# looks up a node number by name, maybe optimize with a dict if needed
+def lookup(nodes, name):
+    for i in range(len(nodes)):
+        if nodes[i] == name:
+            return i
+
+# read the input and return as a graph
+# the graph contains a list of nodes where index is node number and value is label,
+# and a a list of edges
+def getGraph():
+    lines = []
+    while True:
+        try:
+            lines.append(input())
+        except EOFError:
+            break
+
+    nodes = []
+    for line in lines:
+        nodes.append(line.split()[0][:-1])
+    nodes.append("out")
+
+    edges = []
+    for line in lines:
+        links = line.split()[1:]
+        thisun = []
+        for link in links:
+            thisun.append(lookup(nodes, link))
+        edges.append(thisun)
+
+    return nodes, edges
+        
+
+# return all paths from given node to ending node
+def waysToReachOut(nodes, edges, n):
+    # base case: we ARE out
+    if nodes[n] == "out":
+        return [[n]]
+
+    # recursive case: go through out edges
+    ways = []
+    for e in edges[n]:
+         pathsFromHere = waysToReachOut(nodes, edges, e)
+         for p in pathsFromHere:
+             ways.append([n] + p)
+    return ways
+
+
+
+def main():
+    nodes, edges = getGraph()
+    ws = waysToReachOut(nodes, edges, lookup(nodes, "you"))
+    print(len(ws))
+
+main()
+
